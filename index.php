d93d5050b228b6570ef190ec55dde44012a7f7f3
ILovePrograming123 <126216555+ILovePrograming123@users.noreply.github.com>
2023-04-19 10:03:12 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<style>
+div{
+  background-color: white;
+  width: 200px;
+  border: 3px solid white;
+  margin-left: 115;
+  margin-right: 115;
+}
+</style>
+</head>
+<body>
+  <?php
+  ?>
+</body>
+</html>
