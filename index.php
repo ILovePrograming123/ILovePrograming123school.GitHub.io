a7b12ed0edf586e9076daefe6fff59bdfd67d07d
ILovePrograming123 <126216555+ILovePrograming123@users.noreply.github.com>
2023-04-27 14:09:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,32 @@
 <style>
 div{
   background-color: white;
-  width: 200px;
+  width: 250px;
+  height: 400px;
   border: 3px solid white;
-  margin-left: 115;
-  margin-right: 115;
+  margin-top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+}
+body{
+	background-image: url("background.avif");
+	background-size: 100% 100%;
+	background-repeat: no-repeat;
 }
 </style>
 </head>
 <body>
+<div>
+	<h2 style="text-align: center; font-family: monospace; font-size: 30px; margin-top: 15px;margin-bottom: 15;">Login</h2>
+	<hr>
+	<p style="margin-top: 5px;margin-left: 10px;margin-right: 5;font-family: monospace;font-size: 20px;margin-bottom: 10;">Username:</p>
+	<input type="text" name="username" style="height: 25px; width: 220px;margin-left: 10;margin-bottom: 15px">
+	<p style="margin-top: 5px;margin-left: 10px;margin-right: 5;font-family: monospace;font-size: 20px;margin-bottom: 10;">Password:</p>
+	<input type="text" name="password" style="height: 25px; width: 220px;margin-left: 10">
+</div>
   <?php
+	echo <button>Submit</button>;
   ?>
 </body>
 </html>
