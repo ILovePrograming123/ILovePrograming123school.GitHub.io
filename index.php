20f7c72785746d847a88539d676b12d66e26577d
ILovePrograming123 <126216555+ILovePrograming123@users.noreply.github.com>
2023-04-27 17:20:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 div{
   background-color: white;
   width: 250px;
-  height: 250px;
+  height: 310px;
   border: 3px solid white;
   margin-top: 100px;
   margin-left: auto;
@@ -20,6 +20,8 @@ body{
 </head>
 <body>
 <div>
+	<h2 style="text-align: center; font-family: monospace; font-size: 30px; margin-top: 15px;margin-bottom: 15;">Login</h2>
+ 	<hr>
         <p style="margin-top: 5px;margin-left: 10px;margin-right:5; font-family: monospace; font-size: 20px; margin-bottom:10: ">Username:</p>
         <input type="text" name="username" style="height: 25px; width: 220px; margin-left:10;margin-bottom: 15px;">
         <p style="margin-top: 5px;margin-left: 10px;margin-right:5; font-family: monospace; font-size: 20px; margin-bottom:10:">Password:</p>
