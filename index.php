62537a9cbdf92f3a9026ea16bd3fda741b159dd8
ILovePrograming123 <126216555+ILovePrograming123@users.noreply.github.com>
2023-04-27 16:44:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -20,12 +20,11 @@ body{
 </head>
 <body>
 <div>
-	<h2 style="text-align: center; font-family: monospace; font-size: 30px; margin-top: 15px;margin-bottom: 15;">Login</h2>
-	<hr></hr>	
-	<p style="margin-top: 5px;margin-left: 10px;margin-right: 5;font-family: monospace;font-size: 20px;margin-bottom: 10;">Username:</p>
-	<input type="text" name="username" style="height: 25px; width: 220px;margin-left: 10;margin-bottom: 15px;">
-	<p style="margin-top: 5px;margin-left: 10px;margin-right: 5;font-family: monospace;font-size: 20px;margin-bottom: 10;">Password:</p>
-	<input type="text" name="password" style="height: 25px; width: 220px;margin-left: 10">
+        <p style="margin-top: 5px;margin-left: 10px;margin-right:5; font-family: monospace; font-size: 20px; margin-bottom:10: ">Username:</p>
+        <input type="text" name="username" style="height: 25px; width: 220px; margin-left:10;margin-bottom: 15px;">
+        <p style="margin-top: 5px;margin-left: 10px;margin-right:5; font-family: monospace; font-size: 20px; margin-bottom:10:">Password:</p>
+        <input type="text" name="password" style="height: 25px; width: 220px; margin-left:10;">
+        <button type="submit"style="margin-top: 25px; margin-left:75px; margin-right: 75px; height: 50px; width:100px; background-color: #007EF; border-radius: 5px; "><h2 style="font-family: monospace;color :white;">Submit</h2></button>
 	
 	
 </div>
